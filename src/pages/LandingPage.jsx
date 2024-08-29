f643621bdf431290a5ab476f7afbe1fb4d906176
mickey hailu <mickeyhailu20@gmail.com>
2024-08-29 14:44:46 +0300

    added contact form to take name and email of clients

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,7 +5,7 @@ import linkedIn from "../assets/svg/in.svg";
 import instagram from "../assets/svg/instagram.svg";
 import coderIllustraion from "../assets/img/coder-illustration-3.jpg";
 import upworkIcon from "../assets/svg/upwork-icon.svg";
-import fiverrIcon from "../assets/svg/fiverr-icon.svg";
+import fiverrIcon from "../assets/svg/fiverr.svg";
 
 const LandingPage = () => {
   return (
@@ -14,29 +14,46 @@ const LandingPage = () => {
         <div className="flex flex-col md:flex-row md:space-x-10 px-10">
           <div className="w-full md:w-[10%] mb-10 md:mb-0">
             <ul className="flex md:flex-col justify-center md:justify-start space-x-6 md:space-x-0 md:space-y-6">
-              
               <li>
                 <a
                   href="https://github.com/mickey187"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={github} alt="" />{" "}
+                  <img src={github} alt="" width="48" height="48" />{" "}
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/michias-hailu-859135213/">
-                  <img src={linkedIn} alt="" />{" "}
+                <a
+                  href="https://www.linkedin.com/in/michias-hailu-859135213/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={linkedIn} alt="" width="48" height="48" />{" "}
                 </a>
               </li>
               <li>
-                <a href="">
-                  <img src={twitter} alt="" />{" "}
+                <a
+                  href="https://www.upwork.com/freelancers/~019e59c7669fe9fa7c?mp_source=share"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={upworkIcon}
+                    alt=""
+                    className="rounded-full"
+                    width="48"
+                    height="48"
+                  />{" "}
                 </a>
               </li>
               <li>
-                <a href="">
-                  <img src={instagram} alt="" />{" "}
+                <a
+                  href=" https://www.fiverr.com/mickeyhailu1998"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={fiverrIcon} alt="" width="48" height="48" />{" "}
                 </a>
               </li>
             </ul>
@@ -47,7 +64,8 @@ const LandingPage = () => {
               Developer.
             </h1>
             <p className="font-roboto text-lg md:text-xl mt-3">
-            Turning Concepts into High-Performing Websites & Apps. Your Vision, Our Expertise.
+              Turning Concepts into High-Performing Websites & Apps. Your
+              Vision, Our Expertise.
             </p>
             <div className="mt-5">
               <div className="">
@@ -59,9 +77,9 @@ const LandingPage = () => {
                 </Link>
               </div>
               <div>
-                <button className="w-full md:w-48 h-14 rounded-md font-bold bg-transparent mt-4 text-[#000aff] border border-indigo-400">
+                <Link to="/projects" className="w-full md:w-48 h-14 rounded-md font-bold bg-transparent mt-4 text-[#000aff] border border-indigo-400 flex items-center justify-center">
                   View Projects
-                </button>
+                </Link>
               </div>
             </div>
           </div>
